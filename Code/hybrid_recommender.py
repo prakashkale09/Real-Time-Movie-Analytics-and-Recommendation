6149b9ae9e2faa217e4e4a052c601fae5dc3237f
sunbeam <prkashkale202@gmail.com>
2025-07-22 16:42:04 +0530

    Content-based filtering model is completed

--- Code/hybrid_recommender.py
+++ Code/hybrid_recommender.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col,split,explode,udf
+from pyspark.sql.types import DoubleType, ArrayType, StringType
+from pyspark.ml.feature import CountVectorizer, IDF, Normalizer
 spark=SparkSession.builder\
       .appName("HybridMovieRecommender")\
       .getOrCreate()
@@ -8,25 +10,44 @@ spark=SparkSession.builder\
 ratings=spark.read.option("header",True).option("inferSchema",True).csv("hdfs://localhost:9000/user/prakash/moviedata/ratings.csv")
 movies=spark.read.option("header",True).option("inferSchema",True).csv("hdfs://localhost:9000/user/prakash/moviedata/movies.csv")
 
+#EDA
 ratings.show(5)
 movies.show(5)
 movies.printSchema()
 ratings.printSchema()
 
-#step2: Merge the both dataset
-
-data=ratings.join(movies,on="movieId",how="inner")
-
-data.show(5)
-
-#step3: Content-based filtering
-
-movies=movies.withColumn("genre_list", split(col("genres"),"\\|"))
-movies.show(5)
-movies.printSchema()
-
 # Find all genres from genre column
+movies = movies.withColumn("genre_list", split(col("genres"), "\\|"))
 all_genres = movies.select(explode(col("genre_list")).alias("genre"))
 unique_genres=all_genres.distinct()
 unique_genres.show(truncate=False)
 print(unique_genres)
+
+valid_genres = {"Action", "Adventure", "Animation", "Children", "Comedy", "Crime", "Documentary",
+                "Drama", "Fantasy", "Film-Noir", "Horror", "IMAX", "Musical", "Mystery",
+                "Romance", "Sci-Fi", "Thriller", "War", "Western"}
+
+#Remove Generes which are not valid or noisy
+def clean_genre_list(genres):
+    return [g for g in genres if g in valid_genres]
+
+clean_genres_udf = udf(clean_genre_list, ArrayType(StringType()))
+movies = movies.withColumn("genre_list", split(col("genres"), "\\|"))
+movies = movies.withColumn("genre_list", clean_genres_udf(col("genre_list")))
+
+movies.show(5)
+
+#step 2:Content-based filtering
+#Convert the genre_list into numerical vectors
+cv=CountVectorizer(inputCol="genre_list",outputCol="rawFeatures")
+cv_model=cv.fit(movies)
+vectorized=cv_model.transform(movies)
+
+idf=IDF(inputCol="rawFeatures",outputCol="contentFetures")
+idf_model=idf.fit(vectorized)
+content_Features=idf_model.transform(vectorized)
+
+content_Features.show()
+
+normalizer = Normalizer(inputCol="contentFeatures", outputCol="normFeatures")
+movies_content = normalizer.transform(content_Features)
